8092e5df5fa789de5c8f58c76d72adf147de0154
DQE92 <duquedylan@icloud.com>
2025-03-27 00:32:18 +0100

    add 11-12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
     return {
-        allEmployees: { ...employeesList }, // Spread syntax to ensure proper structure
-        getNumberOfDepartments() {
-            return Object.keys(this.allEmployees).length; // Returns the number of departments
-      },
+      allEmployees: employeesList,
+      getNumberOfDepartments() {
+        return Object.keys(this.allEmployees).length;
+        },
     };
 }
