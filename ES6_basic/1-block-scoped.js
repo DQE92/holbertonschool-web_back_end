44c2641e44e7f5a0b85b2a2233025ca486691da2
DQE92 <duquedylan@icloud.com>
2025-03-27 00:01:17 +0100

    1 test

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+    let task = false; let task2 = true;
   
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let task = true; let task2 = false;
   }
-  
   return [task, task2];
 }
