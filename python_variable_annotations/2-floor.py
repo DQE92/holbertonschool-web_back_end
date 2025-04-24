e373110d27628a6395eca41691801873233aadf6
DQE92 <duquedylan@icloud.com>
2025-04-24 15:56:07 +0200

    add tasks

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import math
+
+def floor(n: float) -> float:
+    """
+    Return the floor of a float number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        float: The largest integer less than or equal to n.
+    """
+    return math.floor(n)
---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Return a function that multiplies a float by the given multiplier.
+
+    Args:
+        multiplier (float): The value to multiply by.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float and returns the result of multiplying it by multiplier.
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    
+    return multiplier_function
